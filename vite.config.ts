7429654df0c238b1b54dd5c4ad4eba378e334d50
Pamade <insune15@gmail.com>
2025-10-19 23:51:47 +0200

    fix build issue

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,27 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['pdfjs-dist']
-  },
-  build: {
-    commonjsOptions: {
-      include: [/pdfjs-dist/]
-    }
-  },
   resolve: {
     alias: {
       'pdfjs-dist/build/pdf.worker.min.js': 'pdfjs-dist/build/pdf.worker.mjs'
+    }
+  },
+  optimizeDeps: {
+    include: ['pdfjs-dist', 'react', 'react-dom', 'react-router-dom'],
+    esbuildOptions: {
+      target: 'esnext'
+    }
+  },
+  build: {
+    target: 'esnext',
+    commonjsOptions: {
+      include: [/pdfjs-dist/, /node_modules/],
+      transformMixedEsModules: true
+    },
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
     }
   }
 })
